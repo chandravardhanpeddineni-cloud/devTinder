e8b326409059931af1dc5663929acb1be4889fb4
Chandra Vardhan <chandravardhanpeddineni@gmail.com>
2026-04-23 11:45:54 +0530

    exploring get, post, patch etc..

--- src/app.js
+++ src/app.js
@@ -2,15 +2,39 @@ const express = require('express');
 
 const app = express();   //app is an instance of express js application
 
- app.use('/', (req, res) => {
-    res.send("Welcome to the Dashboard of DevTinder");
- })
-app.use('/hello', (req,res) => {
-    res.send("Hello from the server");
+app.use('/user', (req, res, next) => {
+    console.log("Request received at /user endpoint");
+    next();  //call the next middleware function in the stack
 });
-app.use('/goodbye', (req, res) => {
-    res.send('Goodbye from the server');
+app.patch('/user', (req, res) => {
+   console.log("Updating user in database...");
+   res.send({
+    message: "User updated successfully in database"
+   })
 });
+app.get('/user', (req, res) => {
+        res.send({
+            name: "John Doe",
+            age: 30,
+            email: "john@gmail.com"
+        });
+});
+app.post('/user', (req, res) => {  
+    console.log("Creating user in database...");
+    res.send({
+        message: "User created successfully in database"
+    });
+});
+
+
+app.delete ('/user', (req, res) => {
+    console.log("Deleting user from database...");
+    res.send({
+        message: "User deleted successfully from database"
+    });
+}); 
+
+
 app.listen(3000,() => {
     console.log("Server is running on port 3000");
 }); 
