8dbbd3553189c40fab311d14a45fd3f9ff921daa
Chandra Vardhan <chandravardhanpeddineni@gmail.com>
2026-04-29 16:50:53 +0530

    userSchema-Validation

--- src/app.js
+++ src/app.js
@@ -55,6 +55,43 @@ app.get('/feed', async(req, res) => {
         res.status(500).send("Error fetching feed: " + err);
     }
 })
+
+//Delete user API - DELETE /user - to delete a user by their ID
+app.delete('/user', async(req, res) => {
+    const userId = req.body.userId;
+
+    try{
+        const deleteUser = await User.findByIdAndDelete(userId);
+        // if(!deleteUser) {
+        //     return res.status(404).send("User not found");
+        // }
+        res.status(200).send("User deleted successfully");
+    }
+    catch(err) {
+        res.status(500).send("Error deleting user: " + err);
+    }
+})
+
+//Update user API - PATCh /user - to update a user's details
+app.patch('/user', async(req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const updateUser = await User.findByIdAndUpdate(userId, data, 
+            {
+                returnDocument: "After", 
+                runValidators: true
+            });
+        if(!updateUser) {
+            return res.status(404).send("User not found");
+        }
+        res.status(200).send("User updated successfully");
+    }
+    catch(err) {
+        res.status(500).send("Error updating user: " + err.message);
+    }
+})
+
 connectDB().then(() => {
     console.log("Connected to the database successfully");
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,54 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
     },
     lastName: {
-        type: String
+        type: String,
+        minlength: 3,
+        maxlength: 50
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 6,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+            if(!["male", "female", "other"].includes(value.toLowerCase())) {
+                throw new Error(" Invalid gender value. Allowed values are male, female, or other.");
+            }
+         }
+    },
+    profilePicture: {
+        type: String,
+        default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png"
+    },
+    bio: {
+        type: String,
+        default: "This is a default bio of the user."
+    },
+    skills: {
+        type: [String]
     }
+}, {
+    timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema);
